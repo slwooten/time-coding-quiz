a499932c166401ac19c7b62f503fafe4e2dcb7fd
Seth Wooten <slwooten14@gmail.com>
2022-03-02 18:06:19 -0500

    added variables to select each screen, added variable for take quiz button - when clicked, takes you to active screen - and minor css styling

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
-console.log('we are linked');
+// variables for selecting each screen that is to be displayed at different times //
+var homeEl = document.querySelector('#home');
+var activeEl = document.querySelector('#active');
+var finishedEl = document.querySelector('#finished');
+
+// variable for the take quiz button, used to select the button with id 'take-quiz' //
+var takeQuiz = document.querySelector('#take-quiz');
+
+function homeScreen() {
+    homeEl.style.display = 'flex';
+    activeEl.style.display = 'none';
+    finishedEl.style.display = 'none';
+}
+
+function activeScreen() {
+    homeEl.style.display = 'none';
+    activeEl.style.display = 'flex';
+    finishedEl.style.display = 'none';
+}
+
+function finishedScreen() {
+    homeEl.style.display = 'none';
+    activeEl.style.display = 'none';
+    finishedEl.style.display = 'flex';
+}
+
+
+takeQuiz.addEventListener('click', activeScreen);
