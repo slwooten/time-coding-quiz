5067efe9b1a21175e443a478ab5534d8ee4673f4
Seth Wooten <slwooten14@gmail.com>
2022-03-02 18:25:14 -0500

    added 4 questions with 4 answers each

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,49 @@ var finishedEl = document.querySelector('#finished');
 // variable for the take quiz button, used to select the button with id 'take-quiz' //
 var takeQuiz = document.querySelector('#take-quiz');
 
+var questions = [
+    {
+        question: "Who is the all-time leader for 3-pointers made?",
+        options: [
+            'Ray Allen',
+            'Stephen Curry',
+            'Reggie Miller',
+            "Shaquille O'neal",
+        ],
+        correct: 2
+    },
+    {
+        question: "What player holds the record for most NBA titles won with a total of 11?",
+        options: [
+            'Wilt Chamberlain',
+            'Larry Bird',
+            'Bill Russell',
+            'Magic Johnson',
+        ],
+        correct: 3
+    },
+    {
+        question: "Who is the silhouette in the NBA logo?",
+        options: [
+            'Michael Jordan',
+            'Kobe Bryant',
+            'Bob Cousey',
+            'Jerry West',
+        ],
+        correct: 4
+    },
+    {
+        question: "In what year did the NBA adopt the 3-point line?",
+        options: [
+            '1968',
+            '1981',
+            '1998',
+            '1979',
+        ],
+        correct: 4
+    }
+]
+
 function homeScreen() {
     homeEl.style.display = 'flex';
     activeEl.style.display = 'none';
