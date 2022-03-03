b2a89152e86da53927b11129ca7d5adaaf1a16ac
Seth Wooten <slwooten14@gmail.com>
2022-03-02 20:11:09 -0500

    moved questions to html, linked them to js and made it to where user can click through questions by selecting answers

--- assets/script.js
+++ assets/script.js
@@ -3,51 +3,72 @@ var homeEl = document.querySelector('#home');
 var activeEl = document.querySelector('#active');
 var finishedEl = document.querySelector('#finished');
 
+var q1 = document.querySelector('#question1');
+var q2 = document.querySelector('#question2');
+var q3 = document.querySelector('#question3');
+var q4 = document.querySelector('#question4');
+
 // variable for the take quiz button, used to select the button with id 'take-quiz' //
 var takeQuiz = document.querySelector('#take-quiz');
+var q1Answers = document.querySelector('#q1answers');
+var q2Answers = document.querySelector('#q2answers');
+var q3Answers = document.querySelector('#q3answers');
+var q4Answers = document.querySelector('#q4answers');
+// var questionId = document.querySelector('questions');
 
-var questions = [
-    {
-        question: "Who is the all-time leader for 3-pointers made?",
-        options: [
-            'Ray Allen',
-            'Stephen Curry',
-            'Reggie Miller',
-            "Shaquille O'neal",
-        ],
-        correct: 2
-    },
-    {
-        question: "What player holds the record for most NBA titles won with a total of 11?",
-        options: [
-            'Wilt Chamberlain',
-            'Larry Bird',
-            'Bill Russell',
-            'Magic Johnson',
-        ],
-        correct: 3
-    },
-    {
-        question: "Who is the silhouette in the NBA logo?",
-        options: [
-            'Michael Jordan',
-            'Kobe Bryant',
-            'Bob Cousey',
-            'Jerry West',
-        ],
-        correct: 4
-    },
-    {
-        question: "In what year did the NBA adopt the 3-point line?",
-        options: [
-            '1968',
-            '1981',
-            '1998',
-            '1979',
-        ],
-        correct: 4
-    }
-]
+// var questions = [
+//     {
+//         question: "Who is the all-time leader for 3-pointers made?",
+//         options: [
+//             'Ray Allen',
+//             'Stephen Curry',
+//             'Reggie Miller',
+//             "Shaquille O'neal",
+//         ],
+//         correct: 2
+//     },
+//     {
+//         question: "What player holds the record for most NBA titles won with a total of 11?",
+//         options: [
+//             'Wilt Chamberlain',
+//             'Larry Bird',
+//             'Bill Russell',
+//             'Magic Johnson',
+//         ],
+//         correct: 3
+//     },
+//     {
+//         question: "Who is the silhouette in the NBA logo?",
+//         options: [
+//             'Michael Jordan',
+//             'Kobe Bryant',
+//             'Bob Cousey',
+//             'Jerry West',
+//         ],
+//         correct: 4
+//     },
+//     {
+//         question: "In what year did the NBA adopt the 3-point line?",
+//         options: [
+//             '1968',
+//             '1981',
+//             '1998',
+//             '1979',
+//         ],
+//         correct: 4
+//     }
+// ]
+
+// function showQuestion () {
+//     questionId.innerHTML = '';
+//     var q = questions[0];
+//     for (var i = 0; i < q.options.length; i++) {
+//         var item = q.options[i];
+//         var answerBtn = document.createElement('button');
+//         answerBtn.textContent = i + 1 + ". " + item;
+//         questionId.appendChild(answerBtn);
+//     }
+// }
 
 function homeScreen() {
     homeEl.style.display = 'flex';
@@ -55,10 +76,44 @@ function homeScreen() {
     finishedEl.style.display = 'none';
 }
 
-function activeScreen() {
+function question1() {
     homeEl.style.display = 'none';
     activeEl.style.display = 'flex';
     finishedEl.style.display = 'none';
+    q1.style.display = 'block';
+    q2.style.display = 'none';
+    q3.style.display = 'none';
+    q4.style.display = 'none';
+}
+
+function question2() {
+    homeEl.style.display = 'none';
+    activeEl.style.display = 'flex';
+    finishedEl.style.display = 'none';
+    q1.style.display = 'none';
+    q2.style.display = 'block';
+    q3.style.display = 'none';
+    q4.style.display = 'none';
+}
+
+function question3() {
+    homeEl.style.display = 'none';
+    activeEl.style.display = 'flex';
+    finishedEl.style.display = 'none';
+    q1.style.display = 'none';
+    q2.style.display = 'none';
+    q3.style.display = 'block';
+    q4.style.display = 'none';
+}
+
+function question4() {
+    homeEl.style.display = 'none';
+    activeEl.style.display = 'flex';
+    finishedEl.style.display = 'none';
+    q1.style.display = 'none';
+    q2.style.display = 'none';
+    q3.style.display = 'none';
+    q4.style.display = 'block';
 }
 
 function finishedScreen() {
@@ -67,5 +122,15 @@ function finishedScreen() {
     finishedEl.style.display = 'flex';
 }
 
+function start() {
+    homeScreen();
+}
 
-takeQuiz.addEventListener('click', activeScreen);
+
+takeQuiz.addEventListener('click', question1);
+q1Answers.addEventListener('click', question2);
+q2Answers.addEventListener('click', question3);
+q3Answers.addEventListener('click', question4);
+q4Answers.addEventListener('click', finishedScreen);
+
+start();
